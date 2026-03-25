c7158e70ec728762ea3905f413add85ae18903b9
zyhwan <zyhwan010410@naver.com>
2026-03-25 15:55:20 +0900

    반복자 만들기

--- DynamicArray.h
+++ DynamicArray.h
@@ -4,15 +4,15 @@
 #define __DynamicArray_H__
 #include<stdexcept>
 
-
+template<typename T>
 class DynamicArray
 {
 public:
-	DynamicArray() : Data(new int[1]), Size(0), Capacity(1)
+	DynamicArray() : Data(new T[1]), Size(0), Capacity(1)
 	{
 
 	}
-	DynamicArray(int InitCapacity) : Data(new int[InitCapacity]), 
+	DynamicArray(int InitCapacity) : Data(new T[InitCapacity]),
 		Size(0), Capacity(InitCapacity)
 	{
 
@@ -33,7 +33,7 @@ public:
 			if (Size >= Capacity) //만약에 용량을 넘어서면
 			{
 				Capacity *= 2;
-				int* NewData = new int[Capacity];
+				T* NewData = new T[Capacity];
 				for (int i = 0; i < Size - 1; ++i)
 				{
 					NewData[i] = Data[i];
@@ -43,7 +43,7 @@ public:
 			}
 		}
 	}
-	void Push_Back(const int InValue)
+	void Push_Back(const T& InValue) //값 복사X, 원본 참조O
 	{
 		Size++;
 		ReSize();
@@ -77,7 +77,7 @@ public:
 	//원본 데이터를 복사하는 것은 비용이 너무 크다. 그래서 C++에서는 &래퍼런스 참조를 지원해줘서 원본데이터의 주소를 넘길 수 있다.
 	//&로 리턴해주기 때문에 이 객체의 Data의 주소를 넘겨준다. 그래서 '변수[0] = 데이터 값' 이런식으로 참조할 수 있다.
 	//하지만 const를 int& 앞에 붙여주면 리턴값 변경이 불가하기 때문에 외부에서 반환값을 바꾸지 못한다.
-	const int& operator[](int Index) const
+	const T& operator[](int Index) const
 	{
 		if (Index > Size)
 		{
@@ -87,11 +87,64 @@ public:
 		return Data[Index];
 	}
 
+	void clear()
+	{
+		Size = 0;
+	}
 
-//protected:
-	int* Data;
+protected:
+	T* Data;
 	size_t Size; //size_t <- 컴파일러의 비트수에 따라 데이터의 크기가 다르기 때문에 이를 맞춰주기 위해 이미 정의된 int형 자료형이다.
 	size_t Capacity;
+
+public:
+	//반복자 만들기.--------------------------------------------------------------------
+	class Iterator
+	{
+	public:
+		Iterator(T* InPointer) : Pointer(InPointer)
+		{
+
+		}
+		~Iterator()
+		{
+
+		}
+		//전위 증가
+		Iterator operator++()
+		{
+			Pointer++;
+			return *this;
+		}
+		//후위 증가
+		Iterator operator++(int)
+		{
+			Pointer++;
+			return Iterator(Pointer);
+		}
+		inline bool operator!=(const Iterator& Other)
+		{
+			return Pointer != Other.Pointer;
+		}
+		T& operator*()
+		{
+			return *Pointer;
+		}
+	protected:
+		T* Pointer;
+	};
+	//--------------------------------------------------------------------
+public:
+	Iterator begin()
+	{
+		return Iterator(Data);
+	}
+	Iterator end()
+	{
+		return Iterator(Data + Size);
+	}
+
+
 };
 
 #endif //__DynamicArray_H__
--- main.cpp
+++ main.cpp
@@ -5,19 +5,66 @@
 
 using namespace std;
 
+
+class FVector2D
+{
+public:
+	FVector2D(int x = 0, int y = 0) : x(x), y(y)
+	{
+
+	}
+	~FVector2D()
+	{
+
+	}
+
+	const FVector2D& operator+(FVector2D& rhs)
+	{
+		FVector2D NewVector2D(*this);
+		NewVector2D.x = x + rhs.x;
+		NewVector2D.y = y + rhs.y;
+
+		return NewVector2D;
+	}
+
+
+public:
+	int x;
+	int y;
+};
+
+
+
 int main()
 {
-	DynamicArray OurArray;
+	//FVector2D v1(1, 1);
+	//FVector2D v2(2, 2);
+	//FVector2D v3(v1 + v2);
 
-	try
-	{
-		OurArray.erase(10);
-	}
-	catch (std::out_of_range e)
-	{
-		cout << e.what() << endl;
-	}
+	//v1 = v2 + v3;
+
+	//cout << v1.x << ", " << v1.y << endl;
+	//cout << v3.x << ", " << v3.y << endl;
 	
+
+	DynamicArray<int> V;
+
+	V.Push_Back(1);
+	V.Push_Back(3);
+	V.Push_Back(2);
+	V.Push_Back(4);
+	V.Push_Back(5);
+
+	for (DynamicArray<int>::Iterator iter = V.begin(); iter.operator!=(V.end()); iter++)
+	{
+		cout << *iter << endl;
+	}
+
+	//범위 기반 반복문을 사용하기 위해서는 begin, end 반복자가 필요하다. 그래서 메서드의 이름을 대문자로 만들지 않고 기준에 맞춰서 만들어줘야한다.
+	for (auto i : V)
+	{
+		cout << i << endl;
+	}
 
 	return 0;
 }
