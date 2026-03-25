258554d7ee0d90210bfbc8814ec42db42604d328
zyhwan <145274494+zyhwan@users.noreply.github.com>
2026-03-25 21:36:10 +0900

    +연산자 구현

--- String.h
+++ String.h
@@ -30,12 +30,13 @@ public:
 	}
 
 	//복사 생성자
-	explicit String(const String& rhs) //읽기 전용으로 다른 객체를 가져옴.
+	String(const String& rhs) //읽기 전용으로 다른 객체를 가져옴.
 	{
 		length = rhs.length;
 		Alloc();
 		Copy(rhs);
 	}
+
 	// =연산자 오버로딩
 	String& operator=(const char* rhs)
 	{
@@ -50,7 +51,7 @@ public:
 	}
 	String& operator=(const String& rhs)
 	{
-		length = GetLength(rhs.STR);
+		length = rhs.length;
 		if (STR != nullptr)
 		{
 			delete[] STR;
@@ -64,29 +65,39 @@ public:
 	const char* operator+(const char* rhs)
 	{
 		int len = length + GetLength(rhs);
+		int OtherLength = GetLength(rhs);
 		char* TempString = new char[len + 1];
 		
 		for (int i = 0; i < length; ++i)
 		{
 			TempString[i] = STR[i];
 		}
-		int j = 0;
-		for (int i = length; i < len; ++i)
+		for (int i = 0; i < OtherLength; ++i)
 		{
-			TempString[i] = rhs[j];
-			j++;
+			TempString[i + length] = rhs[i];
 		}
 		TempString[len] = '\0';
 
 		return TempString;
 	}
-	String& operator+(String& rhs)
+
+	String operator+(const String& other)
 	{
 		String TempString;
+		int OtherLength = GetLength(other);
+		TempString.length = length + OtherLength;
+		TempString.Alloc();
 
-		
+		for (int i = 0; i < length; ++i)
+		{
+			TempString.STR[i] = STR[i];
+		}
+		for (int i = 0; i < OtherLength; ++i)
+		{
+			TempString.STR[i + length] = other.STR[i];
+		}
 
-
+		TempString.STR[TempString.length] = '\0';
 
 		return TempString;
 	}
@@ -127,27 +138,8 @@ public:
 		STR[length] = '\0';
 	}
 
-	//뒤에 추가하기
-	void AddString(const char* rhs, int Length)
-	{
-		for (int i = length; i < Length; ++i)
-		{
-			STR[i] = rhs[i];
-		}
-		STR[Length] = '\0';
-		length = Length;
-	}
-	void AddString(const String& rhs, int StartLen)
-	{
-		for (int i = StartLen; i < length; ++i)
-		{
-			STR[i] = rhs.STR[i];
-		}
-		STR[length] = '\0';
-	}
-
 	//길이 반환
-	int GetLength(const char* rhs)
+	int GetLength(const char* rhs) const
 	{
 		int len = 0;
 		for (int i = 0; rhs[i] != '\0'; ++i)
@@ -156,7 +148,7 @@ public:
 		}
 		return len;
 	}
-	int GetLength(const String& rhs)
+	int GetLength(const String& rhs) const
 	{
 		int len = 0;
 		for (int i = 0; rhs.STR[i] != '\0'; ++i)
@@ -165,10 +157,7 @@ public:
 		}
 		return len;
 	}
-
-
 private:
-
 	char* STR; //문자 포인터
 	int length;//문자열 길이
 	int prev_len;
--- main.cpp
+++ main.cpp
@@ -12,13 +12,15 @@ int main()
 	String s1("Hello");
 	String s2("Hello2");
 	String s3(s1);
+	String s4;
 
-	s1 = s2;
 	s1 = s2 + "abc";
+	s4 = s1 + s2;
 
 	cout << s1.GetStr() << endl;
 	cout << s2.GetStr() << endl;
 	cout << s3.GetStr() << endl;
+	cout << s4.GetStr() << endl;
 
 	return 0;
 }
