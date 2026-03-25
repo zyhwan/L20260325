7e01884da012067d944f431bf244baa995f1c6f6
zyhwan <zyhwan010410@naver.com>
2026-03-25 17:50:46 +0900

    MyString Class

---
+++ String.h
@@ -0,0 +1,159 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+
+class String
+{
+public:
+	String() : STR(nullptr), length(0), prev_len(0)
+	{
+
+	}
+	~String()
+	{
+		std::cout << "소멸자 호출" << std::endl;
+		if (STR != nullptr)
+		{
+			delete[] STR;
+			STR = nullptr;
+		}
+	}
+
+	//기본 생성자
+	String(const char* str)
+	{
+		length = GetLength(str);
+		Alloc();
+		Copy(str);
+	}
+
+	//복사 생성자
+	explicit String(const String& rhs) //읽기 전용으로 다른 객체를 가져옴.
+	{
+		length = rhs.length;
+		Alloc();
+		Copy(rhs);
+	}
+	// =연산자 오버로딩
+	String& operator=(const char* rhs)
+	{
+		length = GetLength(rhs);
+		Alloc();
+		Copy(rhs);
+		return *this;
+	}
+	String& operator=(const String& rhs)
+	{
+		length = GetLength(rhs.STR);
+		if (STR != nullptr)
+		{
+			delete[] STR;
+		}
+		Alloc();
+		Copy(rhs);
+		return *this;
+	}
+
+	//+연산자 오버로딩
+	const char* operator+(const char* rhs)
+	{
+		int len = length + GetLength(rhs);
+		char* TempString = new char[len + 1];
+		Copy(TempString);
+		AddString(TempString, length);
+
+		return TempString;
+	}
+	String& operator+(String& rhs)
+	{
+		char* TempString;
+
+
+
+		return *this;
+	}
+
+	//문자열 받기
+	char* GetStr() const
+	{
+		return STR;
+	}
+	int GetLen()
+	{
+		return length;
+	}
+
+	//메모리 확보하기
+	void Alloc()
+	{
+		STR = new char[length + 1];
+	}
+
+	//복사하기 문자로 받을 때
+	void Copy(const char* rhs)
+	{
+		for (int i = 0; i < length; ++i)
+		{
+			STR[i] = rhs[i];
+		}
+		STR[length] = '\0';
+	}
+
+	//복사하기 String 클래스로 받을 때
+	void Copy(const String& rhs)
+	{
+		for (int i = 0; i < length; ++i)
+		{
+			STR[i] = rhs.STR[i];
+		}
+		STR[length] = '\0';
+	}
+
+	//뒤에 추가하기
+	void AddString(const char* rhs, int StartLen)
+	{
+		for (int i = StartLen; i < length; ++i)
+		{
+			STR[i] = rhs[i];
+		}
+		STR[length] = '\0';
+	}
+	void AddString(const String& rhs, int StartLen)
+	{
+		for (int i = StartLen; i < length; ++i)
+		{
+			STR[i] = rhs.STR[i];
+		}
+		STR[length] = '\0';
+	}
+
+	//길이 반환
+	int GetLength(const char* rhs)
+	{
+		int len = 0;
+		for (int i = 0; rhs[i] != '\0'; ++i)
+		{
+			++len;
+		}
+		return len;
+	}
+	int GetLength(const String& rhs)
+	{
+		int len = 0;
+		for (int i = 0; rhs.STR[i] != '\0'; ++i)
+		{
+			++len;
+		}
+		return len;
+	}
+
+
+private:
+
+	char* STR; //문자 포인터
+	int length;//문자열 길이
+	int prev_len;
+};
+
--- main.cpp
+++ main.cpp
@@ -2,69 +2,23 @@
 #include <iostream>
 #include <vector>
 #include "DynamicArray.h"
+#include "String.h"
 
 using namespace std;
 
 
-class FVector2D
-{
-public:
-	FVector2D(int x = 0, int y = 0) : x(x), y(y)
-	{
-
-	}
-	~FVector2D()
-	{
-
-	}
-
-	const FVector2D& operator+(FVector2D& rhs)
-	{
-		FVector2D NewVector2D(*this);
-		NewVector2D.x = x + rhs.x;
-		NewVector2D.y = y + rhs.y;
-
-		return NewVector2D;
-	}
-
-
-public:
-	int x;
-	int y;
-};
-
-
-
 int main()
 {
-	//FVector2D v1(1, 1);
-	//FVector2D v2(2, 2);
-	//FVector2D v3(v1 + v2);
+	String s1("Hello");
+	String s2("Hello2");
+	String s3(s1);
 
-	//v1 = v2 + v3;
+	s1 = s2;
+	s1 = s2 + "abc";
 
-	//cout << v1.x << ", " << v1.y << endl;
-	//cout << v3.x << ", " << v3.y << endl;
-	
-
-	DynamicArray<int> V;
-
-	V.Push_Back(1);
-	V.Push_Back(3);
-	V.Push_Back(2);
-	V.Push_Back(4);
-	V.Push_Back(5);
-
-	for (DynamicArray<int>::Iterator iter = V.begin(); iter.operator!=(V.end()); iter++)
-	{
-		cout << *iter << endl;
-	}
-
-	//범위 기반 반복문을 사용하기 위해서는 begin, end 반복자가 필요하다. 그래서 메서드의 이름을 대문자로 만들지 않고 기준에 맞춰서 만들어줘야한다.
-	for (auto i : V)
-	{
-		cout << i << endl;
-	}
+	cout << s1.GetStr() << endl;
+	cout << s2.GetStr() << endl;
+	cout << s3.GetStr() << endl;
 
 	return 0;
 }
